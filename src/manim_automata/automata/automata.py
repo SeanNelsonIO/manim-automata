8b3930029ab418d5c10d53f1f700d1ef1c2529c9
Sean Nelson <ll17s2n@leeds.ac.uk>
2022-06-28 17:22:54 +0100

    created initial class structure for automaton, states and transitions. Also extracted specific states and transitions from xml to create automaton object

--- src/manim_automata/automata/automata.py
+++ src/manim_automata/automata/automata.py
@@ -1,3 +1,6 @@
+from xmlrpc.client import boolean
+
+
 class Automata:
     """
     Abstarct class providing attributes and methods for automatas
@@ -33,3 +36,69 @@ class Automata:
     # def __getattribute__(self, __name: str) -> Any:
     #     return self.__dict__[__name]
     # look into these please.
+
+
+class State:
+    def __init__(self, name: str, x: int, y: int, initial: bool, final: bool):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.initial = initial
+        self.final = final
+        #need some logic here for initial and final
+
+class Transition:
+    input_symbols = list[str]
+    transition_link = tuple[int]
+
+    def __init__(self, transition: transition_link, read: input_symbols):
+        self.transition = transition
+        self.read = read
+
+    def transition(self):
+        pass
+
+
+class deterministic_finite_automaton():
+    states = []
+    transitions = []
+
+    def __init__(self, template):
+
+        pass
+
+    def add_transition(self, transition: Transition):
+        self.transitions.append(transition)
+
+    def add_state(self, state: State):
+        self.states.append(state)
+
+    def run(self, input_string: list[str]):
+        validation_response = self.validate_automaton() #returns tuple (bool, message)
+        if validation_response[0] == False:
+            return validation_response[1]
+        
+        for symbol in input_string:
+            self.step(symbol)
+
+        pass
+    
+    def step(self, input_sybol: str, transition: Transition):
+        pass
+
+    def validate_automaton(): #returns true if automaton is correct.
+        #check only one initial state
+        #check there is atleast one final state
+        pass
+
+class input_string():
+    pass
+
+
+class alphabet():
+    pass
+
+
+
+
+
--- src/manim_automata/automata/xml_parser.py
+++ src/manim_automata/automata/xml_parser.py
@@ -1,60 +1,8 @@
-# from lxml import etree
-
-# from io import StringIO, BytesIO
-
-# # print("running with cElementTree")
-
-# xml_test = """<structure>
-# 	<type>fa</type>
-# 	<automaton>
-# 		<!--The list of states.-->
-# 		<state id="0" name="q0">
-# 			<x>24640.0</x>
-# 			<y>21780.0</y>
-# 		</state>
-# 		<state id="1" name="q1">
-# 			<x>92.0</x>
-# 			<y>120.0</y>
-# 			<initial/>
-# 		</state>
-# 		<state id="2" name="q2">
-# 			<x>170.0</x>
-# 			<y>134.0</y>
-# 		</state>
-# 		<state id="3" name="q3">
-# 			<x>238.0</x>
-# 			<y>142.0</y>
-# 			<final/>
-# 		</state>
-# 		<!--The list of transitions.-->
-# 		<transition>
-# 			<from>1</from>
-# 			<to>1</to>
-# 			<read>0</read>
-# 		</transition>
-# 		<transition>
-# 			<from>2</from>
-# 			<to>3</to>
-# 			<read>0</read>
-# 		</transition>
-# 		<transition>
-# 			<from>1</from>
-# 			<to>2</to>
-# 			<read>1</read>
-# 		</transition>
-# 	</automaton>
-# </structure>"""
-
-# tree = etree.parse(StringIO(xml_test))
-# print_tree = etree.tostring(tree.getroot())
-# print(print_tree)
-# # tree = etree.parse(StringIO("testmachine.jff"))
-
-# # etree.tostring(tree.getroot())
+from ast import JoinedStr
 import json
 import xmltodict
 
-
+from automata import deterministic_finite_automaton
 
 def parse_xml_file(file_name):
     """
@@ -62,8 +10,77 @@ def parse_xml_file(file_name):
     then jsonify into a dictionary.
     """
     with open(file_name,'rb') as f:
-        return json.dumps(xmltodict.parse(f))
+        return xmltodict.parse(f)
  
 
+#need some kind of validation of the dictionary
+#Could build the automaton anyway and tell the user if it works or not.
+def create_automaton_from_dictionary(dictionary):
+    #create all the states
+    #create all the transitions
+    #create machine
+    deterministic_finite_automaton = deterministic_finite_automaton(dictionary)
+
+
+
+
+
 if __name__ == "__main__":
+
+    #testing functions
     json_dictionary = parse_xml_file('testmachine.jff')
+    if not isinstance(json_dictionary, dict):
+        exit()
+
+
+    states = json_dictionary["structure"]["automaton"]["state"]
+    transitions = json_dictionary["structure"]["automaton"]["transition"]
+
+    print(states)
+    print(transitions)
+
+    for state in states:
+        #create states
+        pass
+
+    for transition in transitions:
+        #create transitions
+        pass
+
+    # print(list(find_key_in_dictionary(json_dictionary, "transition")))
+    # print(list(gen_dict_extract("transition", json_dictionary)))
+
+
+    
+
+# def find_key_in_dictionary(dictionary, condition, path=None):
+
+#     if path is None:
+#         path = []    
+#     # In case this is a dictionary
+#     if isinstance(dictionary, dict):
+#         for key, value in dictionary.items():
+#             new_path = list(path)
+#             new_path.append(key)
+#             for result in find_key_in_dictionary(value, condition, path=new_path):
+#                 yield result 
+
+#             if condition == key:
+#                 new_path = list(path)
+#                 new_path.append(key)
+#                 yield new_path 
+
+#     # return path
+
+# def gen_dict_extract(key, var):
+#     if hasattr(var,'iteritems'):
+#         for k, v in var.iteritems():
+#             if k == key:
+#                 yield v
+#             if isinstance(v, dict):
+#                 for result in gen_dict_extract(key, v):
+#                     yield result
+#             elif isinstance(v, list):
+#                 for d in v:
+#                     for result in gen_dict_extract(key, d):
+#                         yield result
