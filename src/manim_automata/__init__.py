dcbe7b1d6aba17c2789d64434825d7af2f5eb4e7
Sean Nelson <ll17s2n@leeds.ac.uk>
2022-08-19 14:36:07 +0100

    updated poetry lock

--- src/manim_automata/__init__.py
+++ src/manim_automata/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.1.3"
 __all__ = ["ManimAutomaton", "ManimDeterminsticFiniteAutomaton", "ManimNonDeterminsticFiniteAutomaton"]
+
 from .mobjects.manim_automaton import *
 from .mobjects.manim_determinstic_finite_state_automaton import *
 from .mobjects.manim_non_determinstic_finite_state_automaton import *
